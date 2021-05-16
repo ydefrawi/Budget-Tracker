dfcb0860af29b49e3ab552e4dd4c45fd65f6bbf1
Yousef Defrawi <76498767+ydefrawi@users.noreply.github.com>
2021-05-16 16:45:02 -0500

    changed port to 3000

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 
 const app = express();
@@ -20,9 +20,9 @@ mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/budget',
   {
     useNewUrlParser: true,
+    useFindAndModify: false,
     useUnifiedTopology: true,
     useCreateIndex: true,
-    useFindAndModify: false
   }
 );
 
